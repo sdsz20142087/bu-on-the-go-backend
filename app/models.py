f41308d6338afc5c98bc96f7789d7470a77108bc
rickwayne1125 <1037457338@qq.com>
2023-04-06 01:01:59 -0400

    add apis for group&shared_event functions

--- app/models.py
+++ app/models.py
@@ -1,20 +1,21 @@
 from app.extensions import db
 import datetime, time
 
+
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.String(255), unique=True)
-    full_name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    full_name = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), default=db.func.now(), nullable=False)
-    password = db.Column(db.String(255))
-    user_type = db.Column(db.Enum('student', 'teacher', 'staff'))
+    password = db.Column(db.String(255), nullable=False)
+    user_type = db.Column(db.Enum('student', 'teacher', 'staff'), nullable=False)
 
     def __repr__(self):
         return '<User {}>'.format(self.email)
-    
-    @property
-    def as_dict(self):
-        return { 'user_id': self.user_id, 'email': self.email, 'full_name': self.full_name, 'created_at': self.created_at.astimezone().isoformat(), 'user_type': self.user_type}
+
+    # @property
+    # def as_dict(self):
+    #     return { 'user_id': self.user_id, 'email': self.email, 'full_name': self.full_name, 'created_at': self.created_at.astimezone().isoformat(), 'user_type': self.user_type}
 
 
 class Calendar(db.Model):
@@ -26,9 +27,10 @@ class Calendar(db.Model):
     def __repr__(self):
         return '<Calendar {}>'.format(self.calendar_name)
 
-    @property
-    def as_dict(self):
-        return { 'calendar_id': self.calendar_id, 'user_id': self.user_id, 'calendar_name': self.calendar_name, 'calendar_type': self.calendar_type}
+    # @property
+    # def as_dict(self):
+    #     return { 'calendar_id': self.calendar_id, 'user_id': self.user_id, 'calendar_name': self.calendar_name, 'calendar_type': self.calendar_type}
+
 
 class Event(db.Model):
     event_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -44,11 +46,11 @@ class Event(db.Model):
 
     def __repr__(self):
         return '<Event {}>'.format(self.event_name)
-    
-    @property
-    def as_dict(self):
-        return { 'event_id': self.event_id, 'event_name': self.event_name, 'latitude': self.latitude, 'longitude': self.longitude, 'start_time': self.start_time.astimezone().isoformat(),
-                 'end_time': self.end_time.astimezone().isoformat(), 'repeat_mode': self.repeat_mode, 'priority': self.priority, 'desc': self.desc}
+
+    # @property
+    # def as_dict(self):
+    #     return { 'event_id': self.event_id, 'event_name': self.event_name, 'latitude': self.latitude, 'longitude': self.longitude, 'start_time': self.start_time.astimezone().isoformat(),
+    #              'end_time': self.end_time.astimezone().isoformat(), 'repeat_mode': self.repeat_mode, 'priority': self.priority, 'desc': self.desc}
 
 
 class CalendarEvent(db.Model):
@@ -58,10 +60,10 @@ class CalendarEvent(db.Model):
 
     def __repr__(self):
         return '<CalendarEvent {}{}>'.format(self.calendar_id, self.event_id)
-    
-    @property
-    def as_dict(self):
-        return { 'calendar_id': self.calendar_id, 'event_id': self.event_id }
+
+    # @property
+    # def as_dict(self):
+    #     return { 'calendar_id': self.calendar_id, 'event_id': self.event_id }
 
 
 class SharedEvent(db.Model):
@@ -74,10 +76,11 @@ class SharedEvent(db.Model):
     def __repr__(self):
         return '<SharedEvent {}>'.format(self.shared_event_id)
 
-    @property
-    def __dict__(self):
-        return { 'shared_event_id': self.shared_event_id, 'event_id': self.event_id, 'owner_id': self.owner_id, 'created_at': self.created_at.astimezone().isoformat(),
-                 'checkin_time': self.checkin_time.astimezone().isoformat() }
+    # @property
+    # def __dict__(self):
+    #     return { 'shared_event_id': self.shared_event_id, 'event_id': self.event_id, 'owner_id': self.owner_id, 'created_at': self.created_at.astimezone().isoformat(),
+    #              'checkin_time': self.checkin_time.astimezone().isoformat() }
+
 
 class SharedEventParticipance(db.Model):
     shared_event_id = db.Column(db.Integer, primary_key=True)
@@ -87,9 +90,9 @@ class SharedEventParticipance(db.Model):
     def __repr__(self):
         return '<SharedEventParticipance {}{}>'.format(self.shared_event_id, self.user_id)
 
-    @property
-    def __dict__(self):
-        return { 'shared_event_id': self.shared_event_id, 'user_id': self.user_id, 'status': self.status }
+    # @property
+    # def __dict__(self):
+    #     return { 'shared_event_id': self.shared_event_id, 'user_id': self.user_id, 'status': self.status }
 
 
 class Group(db.Model):
